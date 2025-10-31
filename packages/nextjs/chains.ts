53cfd4ccd991d374ad83539c28d26a3b15896e6e
diananov11 <ohlemon0678@gmail.com>
2025-10-31 15:23:09 +0700

    feat: add oracle and gasless page

--- packages/nextjs/chains.ts
+++ packages/nextjs/chains.ts
@@ -1,6 +1,25 @@
-import { defineChain } from "viem";
+import { defineChain as defineChainThirdweb } from "thirdweb";
+import { defineChain as defineChainDefault } from "viem";
 
-export const liskSepolia = /*#__PURE__*/ defineChain({
+export const liskSepoliaThirdweb = defineChainThirdweb({
+  id: 4202,
+  name: "Lisk Sepolia",
+  nativeCurrency: {
+    name: "Sepolia Ether",
+    symbol: "ETH",
+    decimals: 18,
+  },
+  rpc: "https://rpc.sepolia-api.lisk.com",
+  blockExplorers: [
+    {
+      name: "Blockscout",
+      url: "https://sepolia-blockscout.lisk.com",
+    },
+  ],
+  testnet: true,
+});
+
+export const liskSepolia = /*#__PURE__*/ defineChainDefault({
   id: 4202,
   network: "lisk-sepolia",
   name: "Lisk Sepolia Testnet",
